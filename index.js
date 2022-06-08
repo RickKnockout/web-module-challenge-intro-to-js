df8327fa8b0b2799fe351d88a6a6bdaa656e0631
Rick Kayo <rickkayo@gmail.com>
2022-06-07 22:31:35 -0700

    Finished through task 5

--- index.js
+++ index.js
@@ -298,11 +298,12 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/) {
-  /*add your code here*/
+function miles(km) {
+  console.log(km);
+ let resultMiles = km * 0.621371;
+ console.log (resultMiles);
+ return resultMiles;
 }
-
-
 
 //Task 5b - Feet to CM
 /*
@@ -312,9 +313,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/) {
-  /*add your code here*/
-}
+function feet(cm) {
+ let resultFeet = cm / 30.48;
+ return resultFeet;
+}  
 
 
 
